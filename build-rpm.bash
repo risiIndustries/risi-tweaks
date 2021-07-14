4bd35ac71664d6cda048dd8abd6922ab76e7b27d
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2021-07-13 17:57:51 -0700

    Trying to fix buildscript

--- build-rpm.bash
+++ build-rpm.bash
@@ -1,8 +1,8 @@
 version="0.1"
 dir=($pwd)
 rpmdev-setuptree
-tar -zcvf risi-tweaks-${version}.tar.gz risi-tweaks
-mv risi-tweaks-${version}.tar.gz ~/rpmbuild/SOURCES/
+tar -zcvf risi-tweaks.tar.gz risi-tweaks
+mv risi-tweaks.tar.gz ~/rpmbuild/SOURCES/
 cp risi-tweaks.spec ~/rpmbuild/SPECS/
 cd ~/rpmbuild
 rpmbuild -bb SPECS/risi-tweaks.spec
