fe58c69c8cc950a9ae53a3db8647a234c8cf9a3f
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2021-07-13 21:06:19 -0700

    Small build-rpm.bash fix

--- build-rpm.bash
+++ build-rpm.bash
@@ -5,7 +5,7 @@ cp -R risi-tweaks/ risi-tweaks-${version}/
 tar -zcvf risi-tweaks-${version}.tar.gz risi-tweaks-${version}
 rm -rf risi-tweaks-${version}/
 mv risi-tweaks-${version}.tar.gz ~/rpmbuild/SOURCES/
-cp old-risi-tweaks.spec ~/rpmbuild/SPECS/
+cp risi-tweaks.spec ~/rpmbuild/SPECS/
 cd ~/rpmbuild
-rpmbuild -bb SPECS/old-risi-tweaks.spec
+rpmbuild -bb SPECS/risi-tweaks.spec
 cd ${dir}
