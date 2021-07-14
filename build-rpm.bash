322ffa6656b98787822cb5e296909a9387de1684
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2021-07-13 17:51:57 -0700

    Trying to fix spec file so rpm will build

--- build-rpm.bash
+++ build-rpm.bash
@@ -1,3 +1,9 @@
 version="0.1"
+dir=($pwd)
+rpmdev-setuptree
 tar -zcvf risi-tweaks-${version}.tar.gz risi-tweaks
+mv risi-tweaks-${version}.tar.gz ~/rpmbuild/SOURCES/
+cp risi-tweaks.spec ~/rpmbuild/SPECS/
+cd ~/rpmbuild
 rpmbuild risi-tweaks.spec
+cd ${dir}
